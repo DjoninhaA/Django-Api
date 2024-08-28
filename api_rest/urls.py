a5c591cacd2abf6bbc2f8f984a7eb6a601a673a3
Djonis <djonathan8016@gmail.com>
2024-08-27 21:18:38 -0300

    fix - update wasn´t showing json

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.get_all_users, name='all_users'),
     path('user/<int:id>', views.get_by_id, name='get_user'),
     path('user/create',views.create_user, name='create_user'),
+    path('user/update/<int:id>',views.update_user, name='update_user'),
 ]
--- api_rest/views.py
+++ api_rest/views.py
@@ -11,6 +11,8 @@ from .serializers import UserSerializer
 
 import json 
 
+
+
 @api_view(['GET'])
 def get_all_users(request):
     if request.method == 'GET':
@@ -41,3 +43,37 @@ def create_user(request):
             serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+@api_view(['GET', 'PUT'])
+def update_user(request, id):
+    try:
+        data = User.objects.get(id=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = UserSerializer(data)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        print(data)
+        serializer = UserSerializer(data, data =request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['DELETE'])
+def delete_user(request, id):
+    try: 
+        data = User.objects.get(id=id)
+        data.delete()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    
+    
